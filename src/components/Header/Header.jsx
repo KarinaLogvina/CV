b4e7e1338cad708aa476da03782757ba00939f3f
KarinaLogvina <karinalogvina0@gmail.com>
2021-04-19 10:18:17 +0300

    init: start cv

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <ul>
+                <li>About</li>
+                <li>Expierince</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact Me</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
