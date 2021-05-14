8f97b7efe24abc5267fade7b2bfab4d2ae7d39b4
KarinaLogvina <karinalogvina0@gmail.com>
2021-05-14 19:28:13 +0300

    feat: skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,12 +1,27 @@
-
+import s from './skills.module.css'
 
 const Skills = () => {
     return (
         <>
-            <div className="skills">
-                <h2 className="skills_title">Навыки</h2>
-                <p></p>
-            </div>
+            <section className={s.skills}>
+                <div className={s.skills_description}>
+                    <span className={s.skills_title}>Skills</span>
+                    <p>I am inspired by creating great work with people who are as passionate as I am about building something awesome.</p>
+                    <div className={s.zigzag}>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+                <ul className={s.skills_list}>
+                    <div>
+                        <h4 className={s.skill}>Developing</h4>
+                        <p className={s.skills_list_item}>Javascript, ReactJS, ContextAPI, HTML5, CSS3, SASS, BEM, Bootstrap4, TailwindCSS, MaterialUI, Git</p>
+                    </div>
+                    <div>
+                        <h4 className={s.skill}>Social</h4>
+                        <p className={s.skills_list_item}>Effective communication, Conflict resolution, Cooperation, Good Time Management</p>
+                    </div>
+                </ul>
+            </section>
         </>
     )
 }
