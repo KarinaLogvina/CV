ae474a973e8e2271cf313304b24a2417d06e81b7
KarinaLogvina <karinalogvina0@gmail.com>
2021-05-14 19:26:13 +0300

    feat: create header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,8 +7,7 @@ const Header = () => {
                 <h1 className={s.title}>Karina</h1>
                 <nav>
                     <ul className={s.navigation}>
-                        <li className={s.navigation_item}>About</li>
-                        <li className={s.navigation_item}>Expierince</li>
+                        <li className={s.navigation_item}>Home</li>
                         <li className={s.navigation_item}>Skills</li>
                         <li className={s.navigation_item}>Projects</li>
                         <li className={s.navigation_item}>Contact Me</li>
